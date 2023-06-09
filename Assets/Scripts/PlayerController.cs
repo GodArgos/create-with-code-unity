7d222a203b67160ffdc06e5858cd7a49ca05e3d2
Joaquin Incio <joaquin.incio2002@outlook.com>
2023-06-08 22:03:06 -0500

    Lección 1.4 completada

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);   
+        // Get Inputs from certain Axis.
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Moves Player forward or backward a certain speed in 1 second.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput); 
+
+        // Rotates Player to the right or left, by rotating its Y axis, a certain degree in 1 second.
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
